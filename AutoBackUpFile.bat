03f5d27ed317af42faab4bae4caf3409b990b893
Ashish singh chouhan <Aasheesh,saasheesh123@gmail.com>
2023-11-21 23:10:34 +0530

    Daily Commit

--- AutoBackUpFile.bat
+++ AutoBackUpFile.bat
@@ -1,4 +1,5 @@
+set commitMessage=Commit_by_schedual1_%date%;
+git checkout -b %commitMessage%
 git add -A
-set commitMessage="Commit by schedual %date% %time%";
-git commit -m %commitMessage%
-git push origin master
+git commit -m "Daily Commit"
+git push origin %commitMessage%
